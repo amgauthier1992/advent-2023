ecb682af02768af44ee2170c94c77ff3f1eba134
Andy <amgauthier1992@gmail.com>
2023-12-03 17:38:14 -0500

    completed first prompt

---
+++ day-1/index.js
@@ -0,0 +1,15 @@
+import fs from 'fs';
+
+function findCalibrationValues(file) {
+	const lines = fs.readFileSync(file, 'utf-8').trim().split('\n');
+
+  const values = lines.map((line) => {
+		let first = line.split('').find((value) => !Number.isNaN(Number(value)));
+		let last = line.split('').findLast((value) => !Number.isNaN(Number(value)));
+		return Number(first + last);
+	})
+
+	return values.reduce((sum, value) => sum + value);
+}
+
+console.log(findCalibrationValues('./input.txt'));
