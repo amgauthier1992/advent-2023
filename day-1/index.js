215d3cd859a854151a06fe7f430bc1ad9e14286b
Andy <amgauthier1992@gmail.com>
2023-12-03 17:40:24 -0500

    fixes formatting

--- day-1/index.js
+++ day-1/index.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 function findCalibrationValues(file) {
 	const lines = fs.readFileSync(file, 'utf-8').trim().split('\n');
 
-  const values = lines.map((line) => {
+  	const values = lines.map((line) => {
 		let first = line.split('').find((value) => !Number.isNaN(Number(value)));
 		let last = line.split('').findLast((value) => !Number.isNaN(Number(value)));
 		return Number(first + last);
